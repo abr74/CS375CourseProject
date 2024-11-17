92ceb197a9fae88f5ccac87374aaec5e14c3c348
Basim Quadir <bq36@drexel.edu>
2024-11-16 22:37:48 -0500

    pusheed code and assets for speedboost item

--- game.js
+++ game.js
@@ -63,7 +63,7 @@ document.onreadystatechange = function () {
         var itemLayer = game.createLayer('items');
         var coin = itemLayer.createEntity();
         coin.pos = { x: 400, y: 150 };
-        coin.size = { width: 12, height: 16 };
+        coin.size = { width: 16, height: 16 };
         coin.asset = new PixelJS.AnimatedSprite();
         coin.asset.prepare({
             name: 'coin.png',
@@ -75,6 +75,27 @@ document.onreadystatechange = function () {
  
         var collectSound = game.createSound('collect');
         collectSound.prepare({ name: 'coin.mp3' });
+        
+        var powerupLayer = game.createLayer('powerups');
+        var speedBoost = powerupLayer.createEntity();
+        speedBoost.pos = { 
+            x: Math.floor(Math.random() * (700 - 100 + 1) + 100),
+            y: Math.floor(Math.random() * (500 - 100 + 1) + 100)
+        };
+        
+        speedBoost.size = { width: 16, height: 16 };
+        speedBoost.asset = new PixelJS.AnimatedSprite();
+        speedBoost.asset.prepare({
+            name: 'coin.png',
+            frames: 8,
+            rows: 1,
+            speed: 80,
+            defaultFrame: 0
+        });
+
+        var powerupSound = game.createSound('powerup');
+        powerupSound.prepare({ name: 'powerup.wav' }); 
+
         
         player.onCollide(function (entity) {
             if (entity === coin) {
@@ -94,17 +115,58 @@ document.onreadystatechange = function () {
                     '#FFFFFF',
                     'left'
                 );
+            } else if (entity === speedBoost) {
+                powerupSound.play();
+
+                var originalVelocityX = player.velocity.x;
+                var originalVelocityY = player.velocity.y;
+                
+                player.velocity.x == 200;
+                player.velocity.y *= 2;
+                
+                isSpeedBoosted = true;
+                speedBoostTimer = 10;  
+                
+                speedBoost.pos = {
+                    x: Math.floor(Math.random() * (700 - 100 + 1) + 100),
+                    y: Math.floor(Math.random() * (500 - 100 + 1) + 100)
+                };
+                
+                setTimeout(function() {
+                    player.velocity.x = originalVelocityX;
+                    player.velocity.y = originalVelocityY;
+                    isSpeedBoosted = false;
+                    speedBoostTimer = 0;
+                }, 10000);
+
             }
         });
         
         playerLayer.registerCollidable(player);
         itemLayer.registerCollidable(coin);
+        powerupLayer.registerCollidable(speedBoost);
         
         var score = 0;
+        var speedBoostTimer = 0;  
+        var isSpeedBoosted = false;  
         var scoreLayer = game.createLayer("score");
         scoreLayer.static = true;
         
         game.loadAndRun(function (elapsedTime, dt) {
+            if (isSpeedBoosted) {
+                speedBoostTimer -= dt;  
+                
+                scoreLayer.redraw = true;
+                scoreLayer.drawText(
+                    'Speed Boost: ' + Math.ceil(speedBoostTimer) + 's', 
+                    160, 
+                    80, 
+                    '14pt "Trebuchet MS", Helvetica, sans-serif', 
+                    '#FFFF00',  
+                    'right'
+                );
+            }
+            
         });
     }
 }
